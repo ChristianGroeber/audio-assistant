00b9a304d24d1b91ce0c3df5b4507819500a1ba6
ChristianGroeber <christian.groeber@bluewin.ch>
2021-05-26 10:44:09 +0200

    converting 24bit lossles to 16bit lossless

---
+++ 24bit_to_16bit.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+import shutil
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', help='Which Directory')
+args = parser.parse_args()
+
+folder = args.d
+os.chdir(folder)
+
+if os.path.isdir('16bit'):
+    shutil.rmtree('16bit')
+os.mkdir('16bit')
+
+for file in os.listdir():
+    name, ext = os.path.splitext(file)
+    if ext != '.flac':
+        continue   
+    print("Now converting " + file)
+    os.system("sox \"" + file + "\" -b16 \"16bit/" + file + "\"")
