9a225cdf56ff10630f5424388cd2030a9e893878
ChristianGroeber <christian.groeber@bluewin.ch>
2021-05-30 20:18:51 +0200

    properly converting 24bit to 16bit

--- 24bit_to_16bit.py
+++ 24bit_to_16bit.py
@@ -10,13 +10,19 @@ args = parser.parse_args()
 folder = args.d
 os.chdir(folder)
 
-if os.path.isdir('16bit'):
-    shutil.rmtree('16bit')
-os.mkdir('16bit')
+os.chdir('..')
+parent_dir = os.getcwd()
 
+old = folder + ' (24bit)'
+
+shutil.copytree(folder, old)
+os.chdir(folder)
+os.system('rm *.flac')
+
+os.chdir(old)
 for file in os.listdir():
     name, ext = os.path.splitext(file)
     if ext != '.flac':
         continue   
     print("Now converting " + file)
-    os.system("sox \"" + file + "\" -b16 \"16bit/" + file + "\" rate -v -L 44100 dither")
+    os.system("sox \"" + file + "\" -b16 \"" + folder + "/" + file + "\" rate -v -L 44100 dither")
